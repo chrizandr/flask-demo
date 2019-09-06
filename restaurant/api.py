3f0dbcebb0fce744b1be7a6456cfb21650f3c31c
Chris Andrew <chrisandrew119@gmail.com>
2019-09-06 21:27:06 +0530

    Mongodb instead of python storage

--- restaurant/api.py
+++ restaurant/api.py
@@ -4,60 +4,66 @@ from flask import (
     redirect, url_for, flash, jsonify
 )
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from format import menu, order_form
 import pdb
 
 app = Flask(__name__)
 # This is the password to encode/decode the token
 app.secret_key = "DFDKFNWEFOWEFIWV"
-orders = []
-latest_order_id = 0
+app.config["MONGO_URI"] = "mongodb://localhost:27017/demodb"
+# The connection to the MongoDB server, this is common for multiple threads of the app
+mongo = PyMongo(app)
 
 
 @app.route("/menu", methods=["GET"])
 def getmenu():
-    menu_obj = menu.copy()
+    menu_obj = []
+    for x in mongo.db.menu.find():
+        x['_id'] = str(x['_id'])
+        menu_obj.append(x)
+
     return jsonify(menu_obj)
 
 
 @app.route("/order", methods=["GET"])
 def order():
-    global latest_order_id, orders
     items = request.args.getlist('item')
     if len(items) == 0:
         return jsonify({"message": "This is a bad request"}), 400
     else:
         for item in items:
-            # Make a new order for every item
-            new_order = order_form.copy()
-            new_order["id"] = latest_order_id + 1
-            new_order["item"] = item
-            latest_order_id = latest_order_id + 1
-            # add the new order to the orders list
-            orders.append(new_order)
+            menu_item = mongo.db.menu.find_one({"name": item})
+            if menu_item is None:
+                return jsonify({"message": "This is a bad request"}), 400
+            else:
+                new_order = order_form.copy()
+                new_order["name"] = item
+                new_order["price"] = menu_item["price"]
+                # add the new order to the orders list
+                mongo.db.orders.insert(new_order)
         # Give confirmation message to the customer
         return jsonify({"message": "Your food is being prepared in the kitchen"})
 
 
 @app.route("/order", methods=["DELETE"])
 def delete_order():
-    global orders
     delete_order_ids = request.args.getlist('orderid')
-    delete_order_ids = [int(x) for x in delete_order_ids]
 
-    new_order_list = []
-    for order in orders:
-        if order["id"] not in delete_order_ids:
-            new_order_list.append(order)
-
-    orders = new_order_list
+    for order_id in delete_order_ids:
+        mongo.db.orders.remove({"_id": ObjectId(order_id)})
 
     return jsonify({"message": "Your order has been cancelled"})
 
 
 @app.route("/orders", methods=["GET"])
 def get_all_orders():
-    global orders
+    # orders = [x for x in mongo.db.orders.find()]
+    orders = []
+    for x in mongo.db.orders.find():
+        x['_id'] = str(x['_id'])
+        orders.append(x)
+
     return jsonify({"orders": orders})
 
 
@@ -65,22 +71,24 @@ def get_all_orders():
 def pay_for_item():
     global orders
     order_ids = request.args.getlist('orderid')
-    order_ids = [int(x) for x in order_ids]
+    order_ids = [x for x in order_ids]
 
     money = request.form.copy()
     total = get_total(money)
     total_due = 0
 
+    orders = [x for x in mongo.db.orders.find()]
     for order in orders:
-        if order["id"] in order_ids:
+        if str(order['_id']) in order_ids:
             total_due = total_due + order["price"]
 
-    if total != total_due:
+    if total < total_due:
         return jsonify({"message": "Money is wrong"}), 400
     else:
         for order in orders:
-            if order["id"] in order_ids:
-                order["paid"] = True
+            if str(order['_id']) in order_ids:
+                mongo.db.orders.remove({"_id": order["_id"]})
+
         return jsonify({"message": "Food has been paid"}), 200
 
 
@@ -91,6 +99,7 @@ def get_total(money):
 
     total_amount = euro_1 + 5*euro_5 + 10*euro_10
     return total_amount
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
--- restaurant/format.py
+++ restaurant/format.py
@@ -1,23 +1,36 @@
+
 menu = {
     "menu": [
         {
-          "item": "fish",
+          "name": "fish",
           "price": 20
         },
         {
-          "item": "chicken",
+          "name": "chicken",
           "price": 20
         },
         {
-          "item": "pasta",
+          "name": "pasta",
           "price": 10
         }
     ]
 }
 
 order_form = {
-    "id": 1,
-    "item": "chicken",
+    "name": "chicken",
     "price": 30,
     "paid": False
 }
+
+
+if __name__ == "__main__":
+    from flask import Flask
+    from flask_pymongo import PyMongo
+    app = Flask(__name__)
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/demodb"
+    mongo = PyMongo(app)
+
+    for item in menu["menu"]:
+        mongo.db.menu.insert(item)
+
+    app.run(host='localhost', port='32400')
